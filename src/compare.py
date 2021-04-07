3fd71ea9e05f3ce798d61b83bb6270f9fa606f00
hollyhbrady <hollyhbrady@gmail.com>
2021-04-07 18:01:09 +0100

    3 passing tests and code

--- src/compare.py
+++ src/compare.py
@@ -1,2 +1,7 @@
-def compare():
-    pass
+def compare(num1, num2):
+    if num1 < num2:
+        return f"{num1} is less than {num2}"
+    if num1 > num2:    
+        return f"{num1} is greater than {num2}"
+    return f"{num1} is the same as {num2}"
+    
